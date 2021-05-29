d60a88f902819a27d63bed2ae61ad3e981a6d69f
kornavol <volosh.andrey@gmail.com>
2021-05-29 22:48:14 +0200

    delete and update fetch

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,3 +1,5 @@
+const chalk = require('chalk')
+
 const tasksModel = require('../model/tasks')
 
 exports.newTask = async (req, res) => {
@@ -23,3 +25,55 @@ exports.getAll = async (req, res) => {
         }
     })
 }
+
+exports.deleteTask = async (req, res) => {
+    const id = req.params.itemID
+
+    await tasksModel.findByIdAndDelete(id, (err, doc) => {
+        if (err) {
+            console.log(chalk(err));
+            res.status(500).send({ status: "failed", message: err })
+        } else {
+            res.send({ status: "success", message: `Task with ${doc._id} was deleted`})
+        }
+    })
+    
+}
+
+exports.updateTask =  async (req, res) => {
+    
+    const task = { ...req.body }
+
+    console.log(req.body );
+    
+    await tasksModel.findByIdAndUpdate(task._id, task,  (err, doc) => {
+        if (err) {
+            console.log(chalk(err));
+            res.status(500).send({ status: "failed", message: err })
+        } else if (!doc)  {
+            console.log(chalk(doc));
+            res.status(500).send({ status: "failed", message: `Task with ${doc._id} not exist` })
+        }
+        
+        else {
+            console.log(chalk(doc));
+            res.send({ status: "success", message: `Task with ${doc._id} was updated`})
+        }
+    })
+    }
+
+// exports.updateContact = async (req, res) => {
+//     console.log(req.body);
+//     const contact = { ...req.body }
+  
+//     contacts.findByIdAndUpdate(contact._id, contact, { upsert: true, runValidators: true }, (err, doc) => {
+//       if (err) {
+//         console.log(err);
+//         res.send({ status: 'failed', message: err });
+//       } else {
+//         console.log(doc);
+//         logModel.findByIdAndUpdate(req.logId, { preData: JSON.stringify(doc), postData: JSON.stringify(contact) }, (err) => { });
+//         res.send(({ status: 'success', message: 'Contact updated successfully' }));
+//       }
+//     });
+//   }
--- router/tasks.js
+++ router/tasks.js
@@ -8,4 +8,12 @@ router.post('/new', taskContr.newTask);
 
 router.get('/all', taskContr.getAll);
 
+// router.delete('/delete:itemID', taskContr.deleteTask);
+
+router.delete("/delete/:itemID", taskContr.deleteTask);
+
+router.post('/update', taskContr.updateTask);
+
+
+
 module.exports = router;
